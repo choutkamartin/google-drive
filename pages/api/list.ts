789fbd7d61700417cd82b4040fa90e6ca1f83779
Martin Choutka <martafiixek@gmail.com>
2021-09-18 15:04:03 +0200

    List and download files API

---
+++ pages/api/list.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  google, // The top level object used to access services
+  drive_v3, // For every service client, there is an exported namespace
+  Auth, // Namespace for auth related types
+  Common, // General types used throughout the library
+} from "googleapis";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Note: using explicit types like `Auth.GoogleAuth` are only here for
+  // demonstration purposes.  Generally with TypeScript, these types would
+  // be inferred.
+  const auth: Auth.GoogleAuth = new google.auth.GoogleAuth({
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    keyFile: "lib/credentials.json",
+    scopes: ["https://www.googleapis.com/auth/drive"],
+  });
+  const drive: drive_v3.Drive = google.drive({
+    version: "v3",
+    auth,
+  });
+
+  // There are generated types for every set of request parameters
+  const listParams: drive_v3.Params$Resource$Files$List = {};
+  const response = await drive.files.list(listParams);
+
+  // There are generated types for the response fields as well
+  const listResults: drive_v3.Schema$FileList = response.data;
+  res.json(listResults);
+};
---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+interface Error {
+  message: string;
+}
+
+export default function index() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [error, setError] = useState<Error>();
+  useEffect(() => {
+    fetch("/api/list")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setFiles(result.files);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {files.map((item) => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
